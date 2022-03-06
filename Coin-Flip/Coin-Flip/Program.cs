e00b0dcc9f82776e631503920b2f750553285c35
pappurathod <prathod4545@gmail.com>
2022-03-06 09:42:55 +0530

    Head and Tail program

---
+++ Coin-Flip/Coin-Flip/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int Coin_Head = 1; //constant
+Random random = new Random();
+int coinCheck = random.Next(0, 2);
+if (coinCheck == Coin_Head)
+{
+    Console.WriteLine("Coin is Head");
+}
+else
+{
+    Console.WriteLine("Coin is Tail");
+}
